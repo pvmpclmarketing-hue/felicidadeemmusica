0cdf5f4d2dfb0fd1709590272f61daff2d1bbf6d
pvmpclmarketing-hue <pvmpclmarketing@gmail.com>
2026-07-27 10:15:19 -0300

    Delete drizzle directory

--- drizzle/0001_complex_hellcat.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `orders` ADD `buyer_name` text;--> statement-breakpoint
-ALTER TABLE `orders` ADD `buyer_phone` text;
--- drizzle/0002_milky_dust.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `orders` ADD `gateway_payment_id` text;
